283633f56b2a03fe0fedb0e6a77ef03085a22f06
AaronKwong929 <AaronKwong929@gmail.com>
2021-07-28 13:39:30 +0800

    fix: 3

--- aaa.js
+++ aaa.js
@@ -10,7 +10,7 @@ const error = msg => console.log(chalk.bgRedBright(chalk.black(msg)));
 const run = (bin, args, opts = {}) =>
   execa(bin, args, {
     stdio: `inherit`,
-    argv0: `--color`,
+    // argv0: `--color`,
     ...opts,
   });
 
