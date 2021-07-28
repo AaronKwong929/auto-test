62fab5bf97d1fd40459eca1648523372a818188f
AaronKwong929 <AaronKwong929@gmail.com>
2021-07-28 14:34:36 +0800

    feat(33): 123
    456

--- aaa.js
+++ aaa.js
@@ -1,13 +1,12 @@
 const chalk = require('chalk');
-chalk.level = 3;
 const execa = require('execa');
-const { prompt } = require('enquirer');
+// const { prompt } = require('enquirer');
 
-const versionRegex = /^\d+\.\d+\.\d+$/;
-const step = msg => console.log(chalk.bgYellowBright(chalk.black(msg)));
-// const success = msg => console.log(chalk.bgGreenBright(chalk.black(msg)));
-const success = msg => console.log(`\x1B[102m\x1B[30m${msg}\x1B[39m\x1B[49m`);
-const error = msg => console.log(chalk.bgRedBright(chalk.black(msg)));
+// const versionRegex = /^\d+\.\d+\.\d+$/;
+const step = msg => console.log(chalk.bgYellowBright(chalk.black(`\n${msg}`)));
+const success = msg =>
+  console.log(chalk.bgGreenBright(chalk.black(`\n${msg}`)));
+const error = msg => console.log(chalk.bgRedBright(chalk.black(`\n${msg}`)));
 
 const run = (bin, args, opts = {}) =>
   execa(bin, args, {
@@ -15,42 +14,42 @@ const run = (bin, args, opts = {}) =>
     ...opts,
   });
 
-const getGitBranch = () => {
-  const res = execa.commandSync('git rev-parse --abbrev-ref HEAD');
-  return res.stdout;
-};
+const getGitBranch = () =>
+  execa.commandSync('git rev-parse --abbrev-ref HEAD').stdout;
 
 async function main() {
-  const currentBranch = getGitBranch();
-
-  step(`\n添加 git 追踪`);
+  step(`添加 git 追踪`);
   await run(`git`, [`add`, `.`]);
   await run(`git-cz`);
   // await run(`git`, [`commit`, `-m`, `111`]);
   await run(`git`, [`push`]); //
   // 切换到 dev 分支并拉取最新代码
-  step('\n切换到 dev 分支并拉取最新代码');
+  step(`切换到 dev 分支并拉取最新代码`);
   await run(`git`, [`checkout`, `dev`]);
   await run(`git`, [`pull`, `origin`, `dev`]);
-
-  // TODO: 处理发生冲突的情况
 
   // 合并提交分支
   await run(`git`, [`merge`, currentBranch]);
 
   // 推送dev
-  step('\n\n合并到 dev 分支完成，推送到远端');
+  success(`合并到 dev 分支完成`);
+
+  step(`推送到远端`);
   await run(`git`, [`push`]);
 
-  success(`\n\n推送 dev 完成，稍后 Jenkins 将启动构建并通知`);
+  success(`推送 dev 完成，稍后 Jenkins 将启动构建并通知`);
 
-  step(`\n\n切换回到 ${currentBranch} 分支`);
+  step(`切换回到 ${currentBranch} 分支`);
   await run(`git`, [`checkout`, currentBranch]);
 
-  success(`\n\n发布测试环境完成`);
+  success(`发布测试环境完成`);
   return;
 }
 
-main().catch(err => {
+const currentBranch = getGitBranch();
+
+main().catch(async err => {
   error(err);
+  step(`切换回到 ${currentBranch} 分支`);
+  await run(`git`, [`checkout`, currentBranch]);
 });
