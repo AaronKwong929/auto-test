4c9d7019bc0e6853be722f9fe6e3d6cbed5cbf28
AaronKwong929 <AaronKwong929@gmail.com>
2021-07-28 15:06:12 +0800

    111

--- aaa.js
+++ aaa.js
@@ -3,11 +3,10 @@ const execa = require('execa');
 // const { prompt } = require('enquirer');
 
 // const versionRegex = /^\d+\.\d+\.\d+$/;
-const step = msg => console.log(chalk.bgYellowBright(chalk.black(`\n${msg}`)));
-const success = msg =>
-  console.log(chalk.bgGreenBright(chalk.black(`\n${msg}`)));
-const notice = msg => console.log(chalk.bgYellow(chalk.black(`\n${msg}`)));
-const error = msg => console.log(chalk.bgRedBright(chalk.black(`\n${msg}`)));
+const step = msg => console.log(chalk.bgYellowBright(chalk.black(msg)));
+const success = msg => console.log(chalk.bgGreenBright(chalk.black(msg)));
+const notice = msg => console.log(chalk.bgYellow(chalk.black(msg)));
+const error = msg => console.log(chalk.bgRedBright(chalk.black(msg)));
 
 const run = (bin, args, opts = {}) =>
   execa(bin, args, {
@@ -21,17 +20,16 @@ const getGitBranch = () =>
 async function main() {
   const { stdout } = await run('git', ['diff'], { stdio: 'pipe' });
   if (stdout) {
-    step(`添加 git 追踪`);
+    step('\n添加 git 追踪');
     await run(`git`, [`add`, `.`]);
     // await run(`git-cz`);
     await run(`git`, [`commit`, `-m`, `111`]);
   } else {
-    notice(`没有更新的文件`);
+    notice('\n没有更新的文件');
   }
 
-  await run(`git`, [`push`]); //
-  // 切换到 dev 分支并拉取最新代码
-  step(`切换到 dev 分支并拉取最新代码`);
+  await run(`git`, [`push`]);
+  step('\n切换到 dev 分支并拉取最新代码');
   await run(`git`, [`checkout`, `dev`]);
   await run(`git`, [`pull`, `origin`, `dev`]);
 
@@ -39,17 +37,17 @@ async function main() {
   await run(`git`, [`merge`, currentBranch]);
 
   // 推送dev
-  success(`合并到 dev 分支完成`);
+  success('\n合并到 dev 分支完成');
 
-  step(`推送到远端`);
+  step('\n推送到远端');
   await run(`git`, [`push`]);
 
-  success(`推送 dev 完成，稍后 Jenkins 将启动构建并通知`);
+  success('\n推送 dev 完成，稍后 Jenkins 将启动构建并通知');
 
-  step(`切换回到 ${currentBranch} 分支`);
+  step('\n切换回到 ${currentBranch} 分支');
   await run(`git`, [`checkout`, currentBranch]);
 
-  success(`发布测试环境完成`);
+  success('\n发布测试环境完成');
   return;
 }
 
@@ -62,5 +60,5 @@ main()
     await run(`git`, [`checkout`, currentBranch]);
   })
   .finally(() => {
-    notice(`完成辣`);
+    notice('\n完成辣');
   });
