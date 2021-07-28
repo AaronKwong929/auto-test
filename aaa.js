9a0553f30b3463828b5491412e8dd4508aca2c3c
AaronKwong929 <AaronKwong929@gmail.com>
2021-07-28 15:41:33 +0800

    feat: 3

--- aaa.js
+++ aaa.js
@@ -1,10 +1,14 @@
 const chalk = require('chalk');
 const execa = require('execa');
 
-const step = msg => console.log(chalk.bgYellowBright(chalk.black(msg)));
-const success = msg => console.log(chalk.bgGreenBright(chalk.black(msg)));
-const notice = msg => console.log(chalk.bgYellow(chalk.black(msg)));
-const error = msg => console.log(chalk.bgRedBright(chalk.black(msg)));
+const step = msg =>
+  console.log(chalk.bgYellowBright(chalk.black(`STEP: ${msg}`)));
+const success = msg =>
+  console.log(chalk.bgGreenBright(chalk.black(`SUCCESS: ${msg}`)));
+const notice = msg =>
+  console.log(chalk.bgYellow(chalk.black(`NOTICE: ${msg}`)));
+const error = msg =>
+  console.log(chalk.bgRedBright(chalk.black(`ERROR: ${msg}`)));
 
 const run = (bin, args = [], opts = { stdio: `inherit` }) =>
   execa(bin, args, opts);
@@ -14,28 +18,28 @@ const getGitBranch = () =>
 
 async function main() {
   if ([`master`, `dev`].includes(currentBranch)) {
-    error(`\n当前处在 ${currentBranch} 分支，请切换到功能分支`);
+    error(`当前处在 ${currentBranch} 分支，请切换到功能分支`);
     return;
   }
   const { stdout } = await run(`git`, [`diff`], { stdio: `pipe` });
   if (stdout) {
-    step(`\n添加 git 追踪`);
+    step(`添加 git 追踪`);
     await run(`git`, [`add`, `-A`]);
     await run(`git-cz`);
-  } else notice(`\n没有更新的文件`);
+  } else notice(`没有更新的文件`);
 
   await run(`git`, [`push`], { stdio: `pipe` });
-  step(`\n切换到 dev 分支并拉取最新代码`);
+  step(`切换到 dev 分支并拉取最新代码`);
   await run(`git`, [`checkout`, `dev`]);
   await run(`git`, [`pull`, `origin`, `dev`]);
 
   // 合并提交分支
-  step(`\n合并到 dev`);
+  step(`合并到 dev`);
   await run(`git`, [`merge`, currentBranch]);
-  success(`\n合并到 dev 分支完成`);
-  step(`\n推送到远端`);
+  success(`合并到 dev 分支完成`);
+  step(`推送到远端`);
   await run(`git`, [`push`], { stdio: `pipe` });
-  success(`\n推送 dev 完成，稍后 Jenkins 将启动构建并通知`);
+  success(`推送 dev 完成，稍后 Jenkins 将启动构建并通知`);
   return;
 }
 
